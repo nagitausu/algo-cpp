0aee7237eb23974387074df35dc425b631e67ec3
nagitausu <nagitaosu.radio@gmail.com>
2019-08-06 02:03:05 +0900

    Test fenwick and add discription

--- fenwick_tree.cpp
+++ fenwick_tree.cpp
@@ -5,12 +5,14 @@ struct BIT {
     vector<T> d;
     BIT(int n): n(n), d(n+1) {}
 
+    // Add x to i(0-indexed)
     void add(int i, T x=1){
         for(i++; i<=n; i+=i&-i){
             d[i] += x;
         }
     }
 
+    // Get sum of [0, i)
     void sum(int i){
         T x = 0;
         for(i++; i>0; i-=i&-i){
@@ -18,4 +20,4 @@ struct BIT {
         }
         return x;
     }
-}
+};
